af7034c16bfa8c2131bfc6432e7a844cf8e1ba17
Patryk Zdral <patrykz13@gmail.com>
2020-06-03 01:38:08 +0200

    added nyt and bbc photo scraper

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, jsonify
 from flask import request
 
+from bbc import download_bbc_image
 from new_york_times import download_nyt_image
 
 app = Flask(__name__)
 
 
-@app.route('/get-image-photo-link', methods=['POST'])
+@app.route('/get-nyt-image', methods=['POST'])
 def get_nyt_image():
     data = request.get_json(silent=True)
-    print(data)
     img = download_nyt_image(data['url'])
-    print(jsonify(img))
     if img is not None:
         return jsonify(
             src=img[0],
@@ -20,6 +19,17 @@ def get_nyt_image():
     else:
         return jsonify()
 
+@app.route('/get-bbc-image', methods=['POST'])
+def get_bbc_image():
+    data = request.get_json(silent=True)
+    img = download_bbc_image(data['url'])
+    if img is not None:
+        return jsonify(
+            src=img[0],
+            title=img[1]
+        )
+    else:
+        return jsonify()
 
 if __name__ == '__main__':
     app.run()
--- bbc.py
+++ bbc.py
@@ -9,23 +9,24 @@ def download_image(image):
     response = requests.get(image[0], stream=True)
     realname = ''.join(e for e in image[1] if e.isalnum())
 
-    file = open("C://images//new-york-times//{}.jpg".format(realname), 'wb')
+    file = open("C://images//bbc//{}.jpg".format(realname), 'wb')
 
     response.raw.decode_content = True
     shutil.copyfileobj(response.raw, file)
     del response
 
 
-def download_nyt_image(url):
+def download_bbc_image(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     aasClass1 = soup.find_all("img")
-    pattern = re.compile("^css-.*$")
+    print(aasClass1)
+    pattern = re.compile("^js-image-.*$")
     image_info = []
     for a in aasClass1:
         photoWithClass = ""
         try:
-            photoWithClass = a["class"][0] 
+            photoWithClass = a["class"][0]
         except KeyError:
             photoWithClass = ''
 
